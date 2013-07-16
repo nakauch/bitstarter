7fcf3e2f37b1568cd065c68a0d51c92b64c2f753
Ubuntu <ubuntu@ip-172-31-46-29.us-west-2.compute.internal>
2013-07-16 22:42:36 +0000

    Added hello from index.html final

--- web.js
+++ web.js
@@ -10,7 +10,7 @@ fs.readFile('index.html', function (err, data) {
 
 app.get('/', function(request, response) {
     
-    response.send(to.string(dataG));
+    response.send(toString(dataG));
 });
 
 var port = process.env.PORT || 5000;
